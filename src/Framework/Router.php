3b0dbd8e803ff68e742720000d21e3dd257bb192
Karol <karol7525@gmail.com>
2024-03-26 21:09:28 +0100

    Router ready

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -8,12 +8,13 @@ class Router
 {
     private array $routes = [];
 
-    public function add($method, $path)
+    public function add(string $method, string $path, array $controller)
     {
         $path = $this->normalizePath($path);
         $this->routes[] = [
             'path' => $path,
-            'method' => strtoupper($method)
+            'method' => strtoupper($method),
+            'controller' => $controller
         ];
     }
 
@@ -23,5 +24,22 @@ class Router
         $path = "/{$path}/";
         $path = preg_replace('#[/]{2,}#', '/', $path);
         return $path;
+    }
+
+    public function dispatch(string $path, string $method)
+    {
+        $path = $this->normalizePath($path);
+        $method = strtoupper($method);
+
+        foreach ($this->routes as $route) {
+            if (!preg_match("#^{$route['path']}$#", $path) || $route['method'] !== $method) {
+                continue;
+            }
+            [$class, $function] = $route['controller'];
+
+            $controllerInstance = new $class;
+
+            $controllerInstance->$function();
+        }
     }
 }
